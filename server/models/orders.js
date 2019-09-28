4450798ed7cfc8724e854723375992c38c1f7ecd
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-09-28 15:18:08 -0400

    update to orders model

--- server/models/orders.js
+++ server/models/orders.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 let orderSchema = new Schema({
     _id: Schema.ObjectId,
     email: String,
-    comments: String
+    promocode: String
 });
 
 module.exports = mongoose.model("users", userSchema);
