9a5573c87750bd3108b101c825c46eae3318a14f
giocare <jessica.adjei102@gmail.com>
2019-09-28 14:50:59 -0400

    Initializing Branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Hello hiii</h1>
       </header>
     </div>
   );
