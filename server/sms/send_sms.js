76cb3880f8b876419a2c63aa4af3803ddab579e2
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-09-29 04:51:44 -0400

    references

--- server/sms/send_sms.js
+++ server/sms/send_sms.js
@@ -4,8 +4,8 @@
 
 require("dotenv").config();
 
-const accountSid = process.env.ACCOUNTSID;
-const authToken = process.env.AUTHTOKEN;
+const accountSid = 'AC527c6cf0845429276f3072186dbe8bd7';
+const authToken = '939b9f625c41bbad6a12ef2af9bee203';
 
 const client = require('twilio')(accountSid, authToken);
 
@@ -13,7 +13,7 @@ client.messages
   .create({
      body: 'Urgent Action Required (2x confirmed)',
      from: '+12056191477',
-     to: '+18057171455'
+     to: '+13474215572'
    })
   .then(message => console.log(message.sid));
 
