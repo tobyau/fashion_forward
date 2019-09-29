5f6d845bde7a3307047cff6ccd0b4ea4befccc1e
Toby <tobyau9000@gmail.com>
2019-09-29 07:34:23 -0400

    dynamically update sections with button click

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,21 +1,57 @@
 import React, { Component } from "react";
 import ContactForm from './ContactForm';
+import Payment from './Payment';
 import OrderSummary from './OrderSummary';
 
 class Checkout extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      section: false,
+      section_button_text: "Next Section",
+      confirmation: false,
+    }
+  }
+  
+  handleClick = () => {
+    if(this.state.section === false) {
+      this.setState({
+        section: true,
+        section_button_text: "Confirm"
+      })
+    }
+    else {
+      this.setState({
+        section_button_text: "Successfully Ordered!",
+        confirmation: true
+      })
+    }
   }
   
   render() {
+    
+    const handleSections = () => {
+      if(this.state.section === true) {
+        return (<Payment />)
+      }
+      else {
+        return <ContactForm />
+      }
+    }
+    
     return(
-      <div className="form-container row">
-        <div className="col-8">
-          <ContactForm />
-        </div>
-        <div className="col-4">
-          <OrderSummary />
+      <div>
+        <p className="section-indicator">Shipping Address > Payment > Confirm</p>
+        <div className="form-container row">
+          <div className="col-8">
+            <div className="checkout-section">
+              <div>{handleSections()}</div>
+            </div>
+            <button type="button" className="btn btn-primary btn-lg btn-block next-section-button" onClick={this.handleClick}>{this.state.section_button_text}</button>
+          </div>
+          <div className="col-4">
+            <OrderSummary />
+          </div>
         </div>
       </div>
     );
