4390b03313916b0288df0e45d712ed11831b0292
PsyCas <parakram.basnet@gmail.com>
2019-09-29 08:11:28 -0400

    login page completed

--- server/app.js
+++ server/app.js
@@ -11,6 +11,15 @@ const compression = require("compression");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+// Passport config
+require('./config/passport.js')(passport);
+
+// server port
 const port = process.env.SERVER_PORT;
     
 app.set('port', port);
@@ -19,6 +28,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(compression());
 app.use(helmet());
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  }));
+  
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
 
 
 let whitelist = new RegExp(process.env.WHITELIST_DOMAINS);
@@ -46,14 +82,14 @@ db.on('error', console.error.bind(console, 'MongoDB connection error \n'));
 
 
 app.get('/', cors(corsOptions), (req, res, next) => {
-    res.status(200).send("<h1>This route is enabled for CORS</h1>");
+    res.status(200).redirect("http://localhost:3000");
 })
 
 app.use(cors(corsOptions));
 
 app.use("/users", require("./api/users.js"));
 app.use("/handler", require("./api/handleCodes.js"));
-
+app.use("/welcome", require("./login/welcome.js"));
 
 require("./models/orders.js");
 require("./models/users.js");
