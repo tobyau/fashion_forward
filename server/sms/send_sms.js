ff3194feded337916a3146998f9d6756ac6e768f
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-09-29 08:56:39 -0400

    Twilio handling update

--- server/sms/send_sms.js
+++ server/sms/send_sms.js
@@ -7,13 +7,49 @@ require("dotenv").config();
 const accountSid = process.env.ACCOUNTSID;
 const authToken = process.env.AUTHTOKEN;
 
+module.exports = (data) => {
+
 const client = require('twilio')(accountSid, authToken);
+
+if( priority === 0 ){
 
 client.messages
   .create({
-     body: 'Urgent Action Required (2x confirmed)',
+     body: `911: ${data.name} requires immediate assistance at ${data.address}`,
      from: '+12056191477',
-     to: '+13474215572'
+     to: '+18057171455'
    })
   .then(message => console.log(message.sid));
+
+} else if ( priority === 1 ) {
+
+client.messages
+  .create({
+     body: `Police: ${data.name} needs a discreet door knock at ${data.address}`,
+     from: '+12056191477',
+     to: '+18057171455'
+   })
+  .then(message => console.log(message.sid));
+
+} else if ( priority === 2 ) {
+
+client.messages
+  .create({
+     body: `Police: ${data.name} may need assistance at ${data.address}`,
+     from: '+12056191477',
+     to: '+18057171455'
+   })
+  .then(message => console.log(message.sid));
+
+} else {
+
+client.messages
+  .create({
+     body: `False Alarm`,
+     from: '+12056191477',
+     to: '+18057171455'
+   })
+  .then(message => console.log(message.sid));
+
+}}
 
